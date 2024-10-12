2d57022a1a2dc76a8145d9d58b582404af3b8fd2
SpoonySimone <92180945+SpoonySimone@users.noreply.github.com>
2024-10-12 13:07:48 +0200

    better conversion

--- images/convert.bat
+++ images/convert.bat
@@ -1,2 +1,5 @@
-ffmpeg -i input.mp4 -vf "fps=30,palettegen=max_colors=256" -y palette.png
-ffmpeg -i input.mp4 -i palette.png -lavfi "fps=30 [x]; [x][1:v] paletteuse=dither=floyd_steinberg" output.gif
+for %%f in (*.mp4) do (
+    ffmpeg -i "%%f" -vf "fps=30,palettegen=max_colors=256" -y palette.png
+    ffmpeg -i "%%f" -i palette.png -lavfi "fps=30 [x]; [x][1:v] paletteuse=dither=floyd_steinberg" "%%~nf.gif"
+    del palette.png
+)
