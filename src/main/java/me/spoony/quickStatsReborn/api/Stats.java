0c34c9f3d245085b9473e786cdf5f0fe33bbcf4c
SpoonySimone <92180945+SpoonySimone@users.noreply.github.com>
2024-10-17 18:40:00 +0200

    hash change test

--- src/main/java/me/spoony/quickStatsReborn/api/Stats.java
+++ src/main/java/me/spoony/quickStatsReborn/api/Stats.java
@@ -24,7 +24,7 @@ public class Stats {
                 if (game.equals("MAIN") || game.equals("LIMBO") || game.equals("DEFAULT") || game.equals("LEGACY") || !ModConfig.autoGame) {
                     throw new NullPointerException("default game");
                 }
-                if (game.contains("_ULTIMATE") && game.contains("BEDWARS_")) {
+                if (game.contains("_ULTIMATE") && game.contains("BEDWARS")) {
                     game = game.replace("_ULTIMATE", "");
                 }
             } catch (Exception e) {
